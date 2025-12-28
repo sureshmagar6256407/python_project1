6199ef095fd3a855d0b1465c12c8be0ddde31a1b
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2025-12-28 15:03:51 +0545

    update

---
+++ qr_code/advanced_qr.py
@@ -0,0 +1,9 @@
+'''import qrcode
+from PIL import Image
+qr  = qrcode.QRCode(version=1,error_correction=qrcode.ERROR_CORRECT_H,box_size=10,border=5,)
+qr.add_data("random your link")
+qr.make(fit=  True)
+img  =qr.make_image(fill_color ="white",back_color  ="black")
+img.save("DILIP ROKA.png")
+'''
+
