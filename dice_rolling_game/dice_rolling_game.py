e9feac3c058faaa2cd228c795e3d41fc02b8431f
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2025-12-28 17:47:35 +0545

    update

---
+++ dice_rolling_game/dice_rolling_game.py
@@ -0,0 +1,16 @@
+import random 
+
+while True : 
+    choice  =input("Roll the dice? (y/n) : ").lower()
+    if choice  == 'y'  : 
+       die1=random.randint(1,6)
+       die2 = random.randint(1,6)
+       print(f"{die1} , {die2}")
+       
+    elif choice  == 'n' : 
+        print("thanks for playing")
+        break
+     
+    else : 
+        print("invalid choice")
+
