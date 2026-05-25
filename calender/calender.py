ae83b915044e74ee613e69352db39d1725ba51bc
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-05-25 20:30:42 +0545

    update calender and mega project

--- calender/calender.py
+++ calender/calender.py
@@ -1,5 +1,12 @@
-import   calendar   
-year   = int (input("Enter the year :: "))  
-month   = int (input("Enter the month :: "))  
+import calendar
 
-print (calendar.month(year, month))
+try:
+    year = int(input("Enter year (e.g., 2024): "))
+    month = int(input("Enter month (1-12): "))
+
+    if 1 <= month <= 12:
+        print("\n", calendar.month(year, month))
+    else:
+        print("Please enter a valid month between 1 and 12.")
+except ValueError:
+    print("Invalid input. Please enter numbers only.")
