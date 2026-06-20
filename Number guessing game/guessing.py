f07323dc819f28a664afe7ba06ec983f0965e996
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-06-20 06:13:07 +0000

    update

---
+++ Number guessing game/guessing.py
@@ -0,0 +1,19 @@
+import random  
+
+class Guessing : 
+    print("Hi! Welcome to the Number Guessing Game.\nYou have 7 chances to guess the number. Let's start!")
+    def __init__ (self, low ,high) : 
+        self.low=  low 
+        self.high  = high  
+
+
+    
+
+
+
+
+
+low = int(input("Enter the Lower Bound: "))
+high = int(input("Enter the Upper Bound: "))
+g1   = Guessing(low,high)
+
